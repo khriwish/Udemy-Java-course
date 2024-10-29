acf8a3ad7a031bfb2e38c4440c40668144d843e5
Abdalfatah Hoorani <aboodhourani123321@gmail.com>
2024-10-29 19:08:44 +0100

    testing the connection

---
+++ Section8/Bedroom/src/Bedroom.java
@@ -0,0 +1,14 @@
+package Section8.Bedroom.src;
+
+
+public class Bedroom {
+
+
+
+        public void printTest(){
+            System.out.println("Hello from Bedroom");
+        }
+
+}
+
+
---
+++ Section8/Bedroom/src/Main.java
@@ -0,0 +1,12 @@
+package Section8.Bedroom.src;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+         Bedroom = new Bedroom();
+
+
+    }
+
+}
