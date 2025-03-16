209f552c580d00a407cf84846216270a3f0bbb84
Abdalfatah Hoorani <aboodhourani123321@gmail.com>
2025-03-16 09:11:35 +0100

    it should work fully now

--- Section8/building_gas_electric_cars/src/Main.java
+++ Section8/building_gas_electric_cars/src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car("2022 Blue Ferrari 296 GTS");
+        runRace(car);
+
+        Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS",
+                15.4, 6);
+        runRace(ferrari);
+
+        Car tesla = new ElectricCar("2022 red Tesla model 3", 15.4, 6);
+        runRace(tesla);
+
+        Car ferrari1 = new HybridCar("2022 black ferrari sf90 Stradale", 16, 8,8 );
+        runRace(ferrari1);
+
+    }
+
+    public static void runRace(Car car) {
+
+        car.startEngine();
+        car.drive();
+    }
 }
